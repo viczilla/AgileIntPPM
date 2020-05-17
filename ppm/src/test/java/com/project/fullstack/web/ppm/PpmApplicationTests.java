c831cdb812d69911939d83ff20c2b66f2ec1b01f
Victor Truong <viczilla@gmail.com>
2020-05-16 22:34:38 -0400

    initial

---
+++ ppm/src/test/java/com/project/fullstack/web/ppm/PpmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fullstack.web.ppm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
